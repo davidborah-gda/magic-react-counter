f19a03cc8b707527ebf0fbe7d0fa8bd4cdbb9b85
David Borah <dborah87@gmail.com>
2018-07-03 20:33:01 -0400

    React backend logic in place for the counter. It works!

--- src/App.js
+++ src/App.js
@@ -9,12 +9,23 @@ class App extends Component {
       count: 0
     };
   }
+
+  increment(){
+    this.setState({
+      count: this.state.count + 1
+    });
+  }
+  decrement(){
+    this.setState({
+      count: this.state.count - 1
+    });
+  }
   render() {
     return (
       <div>
-        <button onClick={() => alert('yayyy')}>+</button>
+        <button onClick={this.increment.bind(this)}>+</button>
         <h1>{this.state.count}</h1>
-        <button onClick={() => alert('noooo')}>-</button>
+        <button onClick={this.decrement.bind(this)}>-</button>
       </div>
     );
   }
