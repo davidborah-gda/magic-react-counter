4b91ea4811527816450fbcdeea6b224c4441780d
David Borah <dborah87@gmail.com>
2018-07-05 18:24:49 -0400

    Created a basic Counter component

---
+++ src/Counter.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    render() {
+        return (
+            <div>Here be a counter!</div>
+        )
+
+    }
+}
+
+export default Counter
